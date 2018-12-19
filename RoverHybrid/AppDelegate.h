2b4889d56bfea39b316d5a35badcc4ede5f6aa6a
Brady Bentley <bradybentley@bradys-macbook-pro.local>
2018-12-19 13:48:27 -0700

    Initial Commit

---
+++ RoverHybrid/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  RoverHybrid
+//
+//  Created by Brady Bentley on 12/19/18.
+//  Copyright © 2018 Brady. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ RoverHybrid/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  RoverHybrid
+//
+//  Created by Brady Bentley on 12/19/18.
+//  Copyright © 2018 Brady. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
